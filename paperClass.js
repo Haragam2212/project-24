77059b53ac898f20ea8e2f1dd66df2e66d476753
Haragam2212 <70091739+Haragam2212@users.noreply.github.com>
2020-10-13 14:25:05 +0530

    Add files via upload

---
+++ paperClass.js
@@ -0,0 +1,20 @@
+class Paper extends BaseClass{
+ constructor(x,y,radius){
+
+    this.body=Bodies.circle(x,y,diameter,options);
+
+    World.add(world,this.body)
+ }   
+display(){
+super.display();
+
+
+
+}
+
+
+
+
+
+
+}
